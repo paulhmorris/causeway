6f660569991799cd295ff852dfa6bad6afe852b2
Paul Morris <paulh.morris@gmail.com>
2025-06-06 20:09:58 -0500

    First attempt live e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig, devices } from "@playwright/test";
 
 const baseURL = process.env.PLAYWRIGHT_TEST_BASE_URL ?? "http://localhost:3000";
+const isCI = process.env.CI;
 
 export default defineConfig({
   testDir: "./test/e2e",
-  timeout: process.env.CI ? 30_000 : 15_000,
-  testIgnore: !process.env.CI ? "./test/e2e/a11y.test.ts" : undefined,
+  timeout: isCI ? 30_000 : 15_000,
+  testIgnore: !isCI ? "./test/e2e/a11y.test.ts" : undefined,
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "list",
+  forbidOnly: !!isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
+  reporter: isCI ? "html" : "list",
   use: {
     baseURL,
-    trace: "on-first-retry",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     // Setup
     {
@@ -65,10 +64,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
     url: baseURL,
-    reuseExistingServer: !process.env.CI,
-    stdout: "ignore",
-    stderr: "pipe",
+    command: "npm run dev",
+    reuseExistingServer: !isCI,
   },
 });
