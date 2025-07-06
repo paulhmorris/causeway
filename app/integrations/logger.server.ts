4f8f564028cace0548a94da0a134baa712648c86
Paul Morris <paulh.morris@gmail.com>
2025-07-05 22:56:51 -0500

    Try better logger setup

--- app/integrations/logger.server.ts
+++ app/integrations/logger.server.ts
@@ -1,33 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import "@axiomhq/pino";
 import pino from "pino";
 
+import "pino-pretty";
 import { CONFIG } from "~/lib/env.server";
 
-const shouldBeVerbose = CONFIG.isDev || CONFIG.isTest;
+const devTransport: pino.TransportSingleOptions = {
+  target: "pino-pretty",
+  options: {
+    colorize: true,
+    ignore: "pid,hostname",
+  },
+};
 
-const transport: pino.LoggerOptions["transport"] = shouldBeVerbose
-  ? {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        ignore: "pid,hostname",
-      },
-    }
-  : CONFIG.isProd || CONFIG.isPreview
-    ? {
+const baseLogger = pino(
+  {
+    transport: CONFIG.isDev ? devTransport : undefined,
+    level: CONFIG.isDev ? "debug" : (process.env.LOG_LEVEL ?? "info"),
+    name: "Global",
+  },
+  CONFIG.isDev
+    ? undefined
+    : pino.transport({
         target: "@axiomhq/pino",
         options: {
           dataset: process.env.AXIOM_DATASET,
           token: process.env.AXIOM_TOKEN,
         },
-      }
-    : undefined;
-
-const baseLogger = pino({
-  transport,
-  level: shouldBeVerbose ? "debug" : (process.env.LOG_LEVEL ?? "info"),
-  name: "Global",
-});
+      }),
+);
 
 export function createLogger(name?: string) {
   return name ? baseLogger.child({ name }) : baseLogger;
